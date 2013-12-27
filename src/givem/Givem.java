0412f17be974a2362db07fc39de4b1123618452b
fabianele1-desktop <fabian.conpir@gmail.com>
2013-12-27 13:52:38 -0500

    Delete

--- src/givem/Givem.java
+++ src/givem/Givem.java
@@ -17,9 +17,6 @@ public class Givem {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Guest modification - Ac");
-        System.out.println("Ok");
-        System.out.println("My modifications");
     }
     
 }
